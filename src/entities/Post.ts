fcaf420aafe4268a4e669a0626c451a9c047dc3d
Preeternal <preeternal@ya.ru>
2020-10-01 16:27:43 +0300

    prettier

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,8 +1,7 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity()
 export class Post {
-
   @PrimaryKey()
   id!: number;
 
