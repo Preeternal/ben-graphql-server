d98fe3359e883903b3bd95c9e6824df451ccbf4a
Preeternal <preeternal@ya.ru>
2020-10-02 15:46:36 +0300

    post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
 
 import { MyContext } from 'src/types';
 import { Post } from '../entities/Post';
@@ -8,5 +8,13 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
