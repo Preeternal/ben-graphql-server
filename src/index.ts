ed9ca077a7a06519e665e75d39d1053d2f1cd7c3
Preeternal <preeternal@ya.ru>
2020-10-07 11:34:32 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { buildSchema } from 'type-graphql';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
+import cors from 'cors';
 
 // import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
@@ -23,6 +24,13 @@ const main = async () => {
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
   app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
+  app.use(
+    // '/',
     session({
       name: 'qid',
       store: new RedisStore({
@@ -50,7 +58,10 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
