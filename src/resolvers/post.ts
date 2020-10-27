b21bf8eeae801787272c43332776be67e9f40dd4
Preeternal <preeternal@ya.ru>
2020-10-27 13:56:49 +0200

    ssr cookie

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -19,6 +19,7 @@ import {
 import { Post } from '../entities/Post';
 import { getConnection } from 'typeorm';
 import { Updoot } from '../entities/Updoot';
+import { registerDecorator } from 'class-validator';
 
 @InputType()
 class PostInput {
@@ -114,10 +115,17 @@ export class PostResolver {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    const replacements: any[] = [realLimitPlusOne, req.session!.userId];
+    const replacements: any[] = [realLimitPlusOne];
+
+    if (req.session!.userId) {
+      replacements.push(req.session!.userId);
+    }
+
+    let cursorIdx = 3;
 
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
+      cursorIdx = replacements.length;
     }
 
     const posts = await getConnection().query(
@@ -137,7 +145,7 @@ export class PostResolver {
       }
       from post p
       inner join public.user u on u.id = p."creatorId"
-      ${cursor ? `where p."createdAt" < $3` : ''}
+      ${cursor ? `where p."createdAt" < $${cursorIdx}` : ''}
       order by p."createdAt" DESC
       limit $1
     `,
